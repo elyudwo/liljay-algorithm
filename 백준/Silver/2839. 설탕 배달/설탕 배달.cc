6b20001fcda1fb29bf4dcd196b51d17aca86f1e3
elyudwo <97587573+elyudwo@users.noreply.github.com>
2023-01-26 14:35:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int N;
+	cin >> N;
+	int m = N / 5;
+	int cnt = 0;
+	int b;
+	while(1)
+	{
+		if(m < 0)
+        {
+            cout << -1;
+            return 0;
+        }
+		
+		if((N - (5*m))%3 == 0)
+		{
+			b = (N-(5*m)) / 3;
+			break;	
+		}
+		m--;
+	}
+	
+	cout << m + b;
+	
+	return 0;
+}

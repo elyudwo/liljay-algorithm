c87d666fa199a35f1890375e00eb88146634a7f0
elyudwo <97587573+elyudwo@users.noreply.github.com>
2023-05-21 00:28:45 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 144 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -1,76 +1,54 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
  
 using namespace std;
 
 long long n,Hatk;
 
-struct s {
-	int num;
-	long long a;
-	long long h;
-};
-
-vector<s> vec;
-
 void input() {
 	cin >> n >> Hatk;
-
-	int a,b,c;
-	
-	for(int i=0; i<n; i++) {
-		cin >> a >> b >> c;
-		vec.push_back({a,b,c});
-	}
 }
 
-bool check(long long mid) {
-	long long tmp = mid;
-	long long atk = Hatk;
+void solve() {
+	long long t,a,h;
+	long long life = 0;
+	long long store_life = 0;
 	
-	for(int i=0; i<vec.size(); i++) {
-		if(vec[i].num == 1) {
-			int cnt = 0;
+	for(int i=0; i<n; i++) {
+		cin >> t >> a >> h;
+		
+		if(t == 1) {
+			long long cnt = 0;
 			
-			if(vec[i].h % atk != 0) { cnt = vec[i].h/atk; }
-			else { cnt = vec[i].h/atk - 1; }
+			if(h % Hatk != 0) { cnt += h / Hatk; }
+			else { cnt = h/Hatk -1; }
 			
-			tmp -= cnt * vec[i].a;
-			if(tmp <= 0) { return false; }
+			long long damage = cnt * a;
+			
+			if(store_life != 0) {
+				if(damage > store_life) {
+					damage -= store_life;
+					store_life = 0;
+				}
+				else {
+					store_life -= damage;
+					damage = 0;
+				}
+			}
+			
+			life += damage;
 		}
-		else {
-			atk += vec[i].a;
-			tmp += vec[i].h;
+		else if(t == 2) {
+			Hatk += a;
+			store_life += h;
 			
-			if(tmp > mid) {
-				tmp = mid;
+			if(store_life > life) {
+				store_life = life;
 			}
 		}
 	}
 	
-	return true;
-}
-
-void solve() {
-	long long left = 0;
-	long long right = 1e18;
-	long long result = -1;
-	
-	while(left <= right) {
-		long long mid = (left + right) / 2;
-		
-		bool tmp = check(mid);
-		// 성공 
-		if(tmp == true) {
-			result = mid;
-			right = mid - 1;
-		}
-		// Hp 더 필요 
-		else { left = mid + 1; }
-	}
-	
-	cout << result;
+	cout << life + 1;
 }
 
 int main() {

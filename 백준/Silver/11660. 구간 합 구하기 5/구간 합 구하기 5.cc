9eaad18b0068ddadebf6db1149cbaf7a744b1c29
elyudwo <97587573+elyudwo@users.noreply.github.com>
2023-11-20 16:36:34 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 132 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,41 +2,27 @@
 
 using namespace std;
 
-int v[1025][1025];
-int dp[1025][1025];
+int arr[1025][1025];
+int sum[1025][1025];
 
-
-int main(void)
-{
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 	int n,m;
 	cin >> n >> m;
 	
 	for(int i=1; i<=n; i++) {
 		for(int j=1; j<=n; j++) {
-			scanf("%d",&v[i][j]);
+			cin >> arr[i][j];
+			sum[i][j] = arr[i][j] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
 		}
 	}
-	
-	
-	for(int i=1; i<=n; i++) {
-		dp[i][1] = v[i][1];
-		for(int j=2; j<=n; j++) {
-			dp[i][j] = dp[i][j-1] + v[i][j];
-		}
-	}
-	
-	
 	
 	for(int i=0; i<m; i++) {
-		int x1,y1,x2,y2;
-		scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
-		int sum = 0;
-		for(int i=x2; i>=x1; i--) {
-			sum = sum + (dp[i][y2] - dp[i][y1-1]);
-		}
-		printf("%d\n",sum);
+		int a,b,c,d;
+		cin >> a >> b >> c >> d;
+		
+		cout << sum[c][d] - sum[a-1][d] - sum[c][b-1] + sum[a-1][b-1] << '\n';
 	}
-	
-	
-	return 0;
 }

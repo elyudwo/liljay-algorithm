ab4a1e5b0c2f741fa4916009700c55a9c9e99410
elyudwo <97587573+elyudwo@users.noreply.github.com>
2024-01-02 16:23:59 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#define MAX 2000000000
 using namespace std;
 
 int n;
@@ -18,55 +18,29 @@ void input() {
 }
 
 void solve() {
-    int answer = 2000000000;
+    int answer = MAX;
     
     for(int i=1; i<=3; i++) {
         int dp[1002][4];
         
         for(int j=1; j<=3; j++) {
-            dp[1][j] = arr[1][i];
+            if(i == j) {
+                dp[1][j] = arr[1][j];
+                continue;
+            }
+            dp[1][j] = MAX;
         }
         
-        for(int j=2; j<=n-1; j++) {
-            for(int k=1; k<=3; k++) {
-                if(j == 2 && k == i) { dp[j][k] = 20000; continue; }
-                if (k == 1) {
-                    dp[j][k] = min(dp[j-1][k+1], dp[j-1][k+2]);
-                }
-                if (k == 2) {
-                    dp[j][k] = min(dp[j-1][k-1], dp[j-1][k+1]);
-                }
-                if (k == 3) {
-                    dp[j][k] = min(dp[j-1][k-1], dp[j-1][k-2]);
-                }
-                dp[j][k] += arr[j][k];
-            }
+        for(int j=2; j<=n; j++) {
+            dp[j][1] = min(dp[j-1][2], dp[j-1][3]) + arr[j][1];
+            dp[j][2] = min(dp[j-1][1], dp[j-1][3]) + arr[j][2];
+            dp[j][3] = min(dp[j-1][1], dp[j-1][2]) + arr[j][3];
         }
         
         for(int j=1; j<=3; j++) {
             if(j == i) { continue; }
-            if (j == 1) {
-                dp[n][j] = min(dp[n-1][j+1], dp[n-1][j+2]);
-            }
-            if (j == 2) {
-                dp[n][j] = min(dp[n-1][j-1], dp[n-1][j+1]);
-            }
-            if (j == 3) {
-                dp[n][j] = min(dp[n-1][j-1], dp[n-1][j-2]);
-            }
-            
-            dp[n][j] += arr[n][j];
             answer = min(answer, dp[n][j]);
         }
-        
-        // cout << endl;
-        // for(int i=1; i<=n; i++) {
-        //     for(int j=1; j<=3; j++) {
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        
     }
     
     cout << answer;

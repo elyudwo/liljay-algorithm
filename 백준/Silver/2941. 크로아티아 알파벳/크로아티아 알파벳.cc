530e639f51f6abde040f4a7d62497859ef835cbd
elyudwo <97587573+elyudwo@users.noreply.github.com>
2024-01-04 15:58:31 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	string str;
+	cin >> str;
+	int idx; 
+	vector<string> croatian = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+	
+	for(int i=0; i < croatian.size(); i++)
+	{
+		while(1)
+		{
+			idx = str.find(croatian[i]);
+			if(idx == string::npos)
+				break;
+			str.replace(idx,croatian[i].length(),"#");
+		}
+	}	
+	cout << str.length();
+	
+	return 0;
+}

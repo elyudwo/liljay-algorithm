90f6e504ea6962d52c1fad0585798678b0c0cf9a
elyudwo <97587573+elyudwo@users.noreply.github.com>
2024-05-31 19:45:30 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.02 ms, Memory: 49.4 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -1,94 +1,55 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <queue>
 #include <iostream>
 
 using namespace std;
 
-vector<int> graph[1000002];
-int in[1000002];
-int out[1000002];
+/*
+1. 생성한 정점 구하기
+2. 생성한 정점과 연결된 점들 다 끊기
+3. 각 그래프 별 개수 구하기
+*/
+
+vector<int> vec_in[1000002];
+vector<int> vec_out[1000002];
 bool check[1000002];
-int donut = 0, line = 0, eight = 0;
+int max_value = 0, donut = 0, line = 0, eight = 0, makePoint;
 
 void makeGraph(vector<vector<int>> edges) {
     for(vector<int> edge : edges) {
         int a = edge[0];
         int b = edge[1];
+        max_value = max({max_value, a, b});
         
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-        out[a] += 1;
-        in[b] += 1;
+        vec_out[a].push_back(b);
+        vec_in[b].push_back(a);
     }
-}
-
-int checkMakePoint() {
-    int makePoint;
-    for(int i=1; i<=1000000; i++) {
-        if(!in[i] && out[i] >= 2) {
-            makePoint = i;
-            check[i] = true;
-            break;
-        }
-    }
-    
-    for(int i=0; i<graph[makePoint].size(); i++) {
-        in[graph[makePoint][i]] -= 1;
-    }
-    
-    return makePoint;
-}
-
-
-void bfs(int x) {
-    queue<int> q;
-    q.push(x);
-    check[x] = true;
-    bool lineB = false, eightB = false, donutB = false;
-    
-    
-    while(!q.empty()) {
-        int nx = q.front();
-        if(in[nx] && !out[nx]) { lineB = true; }
-        if(in[nx] == 2 && out[nx] == 2) { eightB = true; }
-        
-        q.pop();
-        
-        for(int i=0; i<graph[nx].size(); i++) {
-            int tmp = graph[nx][i];
-            if(check[tmp]) { continue; }
-            check[tmp] = true;
-            q.push(tmp);
-        }
-    }
-    if(lineB) { line++; }
-    if(eightB) { eight++; }
-    if(!lineB && !eightB) { donut++; }
-    
 }
 
 vector<int> solution(vector<vector<int>> edges) {
     vector<int> answer;
     makeGraph(edges);
+    int makePoint, donut = 0, line = 0, eight = 0;
     
-    answer.push_back(checkMakePoint());
-    
-    for(int i=0; i<edges.size(); i++) {
-        if(!check[edges[i][0]]) { bfs(edges[i][0]); }
-    }
-    
-    for(int i=0; i<edges.size(); i++) {
-        if(!check[edges[i][0]]) {
+    for(int i=1; i<=max_value; i++) {
+        if(vec_out[i].size() == 0 && vec_in[i].size() >= 1) {
             line++;
-            check[edges[i][0]] = true;
+            continue;
         }
-        if(!check[edges[i][1]]) {
-            check[edges[i][1]] = true;
-            line++;
+        if(vec_in[i].size() >= 2 && vec_out[i].size() >= 2) {
+            eight++;
+            continue;
+        }
+        if(vec_in[i].size() == 0 && vec_out[i].size() >= 2) {
+            makePoint = i;
         }
     }
     
+    donut = vec_out[makePoint].size() - (eight + line);
+    
+    answer.push_back(makePoint);
     answer.push_back(donut);
     answer.push_back(line);
     answer.push_back(eight);

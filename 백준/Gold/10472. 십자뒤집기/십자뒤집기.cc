a17219724ae881c2a45b4c73c536d9022a20d6ad
elyudwo <97587573+elyudwo@users.noreply.github.com>
2024-06-18 16:14:28 +0900

    [Gold V] Title: 십자뒤집기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10472. 십자뒤집기/십자뒤집기.cc
+++ 백준/Gold/10472. 십자뒤집기/십자뒤집기.cc
@@ -5,23 +5,23 @@
 
 using namespace std;
 
-set<string> se;
 char board[4][4];
-bool visit[4][4];
+bool check[(1 << 9)];
 int dx[] = {0,0,0,-1,1};
 int dy[] = {0,-1,1,0,0};
-
 int answer = INF; 
 int tmp = 0;
 
-string makeStr() {
-	string str = "";
-	for(int i=1; i<=3; i++) {
-		for(int j=1; j<=3; j++) {
-			str += board[i][j];
+int makeStrToInt() {
+	int result = 0;
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+			if(board[i+1][j+1] == '*') {
+				result |= (1 << i * 3 + j);
+			}
 		}
 	}
-	return str;
+	return result;
 }
 
 void makeBoard() {
@@ -36,11 +36,14 @@ void makeBoard() {
 
 void init() {
 	answer = INF;
-	se.clear();
+	
+	for(int i=0; i<(1 << 9); i++) {
+		check[i] = false;
+	}
+	
 	for(int i=1; i<=3; i++) {
 		for(int j=1; j<=3; j++) {
 			board[i][j] = '.';
-			visit[i][j] = false;
 		}
 	}
 }
@@ -52,39 +55,35 @@ int main() {
 	while(t--) {
 		init();
 		makeBoard();
-		queue<pair<string, int> > q;
-		string tmp = makeStr();
+		queue<pair<int, int> > q;
+		int tmp = makeStrToInt();
 		
 		q.push({tmp, 0});
+		check[tmp] = true;
 		
 		while(!q.empty()) {
-			string str = q.front().first;
+			int num = q.front().first;
 			int cnt = q.front().second;
 			
 			q.pop();
 			
-			if(str == ".........") {
+			if(num == 0) {
 				cout << cnt << '\n';
 				break;
 			}
 			for(int i=0; i<9; i++) {
-				int x = i / 3;
-				int y = i % 3;
-				string tmp = str;
+				int tmp = num;
 				
 				for(int j=0; j<5; j++) {
-					int nx = x + dx[j];
-					int ny = y + dy[j];
+					int nx = (i / 3) + dx[j];
+					int ny = (i % 3) + dy[j];
 					
 					if(nx < 0 || ny < 0 || nx >= 3 || ny >= 3) continue;
-					int idx = nx * 3 + ny;
-					
-					if(tmp[idx] == '*') { tmp[idx] = '.'; }
-					else { tmp[idx] = '*'; }
+					tmp ^= (1 << (nx * 3 + ny));
 				}
 				
-				if(!se.count(tmp)) {
-					se.insert(tmp);
+				if(!check[tmp]) {
+					check[tmp] = true;
 					q.push({tmp, cnt + 1});
 				}
 			}

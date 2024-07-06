e50b17b16801a713cb9d5020cee5d1a1e11c6bdd
elyudwo <97587573+elyudwo@users.noreply.github.com>
2024-07-07 01:53:44 +0900

    [Gold III] Title: 자석, Time: 276 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/28303. 자석/자석.cc
+++ 백준/Gold/28303. 자석/자석.cc
@@ -1,45 +1,38 @@
 #include <iostream>
+#include <cmath> 
+#include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
 
-ll arr[500003];
-ll sum_ns[500003];
-ll sum_sn[500003];
-
-ll answer_ns[500003];
-ll answer_sn[500003];
-ll answer = -60000000000000;
+ll arr[500002];
+ll answer = -2e18;
 
 int main() {
-	int n, k;
-	cin >> n >> k;
-	
-	for(int i=0; i<n; i++) { cin >> arr[i]; }
-	for(int i=1; i<n; i++) {
-		sum_ns[i-1] = arr[i-1] - arr[i] - k;
-	}
-	for(int i=1; i<n; i++) {
-		sum_sn[i-1] = arr[i] - arr[i-1] - k;
-	}
-	
-	answer_ns[0] = sum_ns[0];
-	answer = max(answer, answer_ns[0]);
-	for(int i=1; i<n-1; i++) {
-		ll tmp = 0;
-		answer_ns[i] = sum_ns[i] + max(tmp, answer_ns[i-1]);
-		answer = max(answer, answer_ns[i]);
-	}
-	
-	answer_sn[0] = sum_sn[0];
-	answer = max(answer, answer_sn[0]);
-	for(int i=1; i<n-1; i++) {
-		ll tmp = 0;
-		answer_sn[i] = sum_sn[i] + max(tmp, answer_sn[i-1]);
-		answer = max(answer, answer_sn[i]);
-	}
-	
-	
-	cout << answer;
+   int n, k;
+   cin >> n >> k;
+   
+   for(int i=1; i<=n; i++) {
+       cin >> arr[i];
+   }
+   ll x = 0;
+   
+   for(int i=1; i<n; i++) {
+       ll tmp = arr[i] - arr[i+1] - k;
+       x = max(x, 0ll);
+       x += tmp;
+       answer = max(answer, x);
+   }
+   
+   x = 0;
+   for(int i=n; i>1; i--) {
+       ll tmp = arr[i] - arr[i-1] - k;
+       x = max(x, 0ll);
+       x += tmp;
+       answer = max(answer, x);
+   }
+   
+   cout << answer;
+   
 }
